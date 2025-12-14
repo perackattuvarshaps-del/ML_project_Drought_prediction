e6e92a65d87a70242757860861013a8a277c076f
Varsha P S <perackattuvarshaps@gmail.com>
2025-12-14 23:11:48 +0530

    for

--- Drought_Prediction/app.py
+++ Drought_Prediction/app.py
@@ -1,5 +1,5 @@
 
-import streamlit
+import streamlit as st
 import pandas as pd
 from matplotlib import pyplot
 from sklearn.model_selection import train_test_split
@@ -9,25 +9,26 @@ from sklearn.metrics import accuracy_score
 
 
  
-streamlit.set_page_config(page_title="Drought Prediction System", layout="wide")
+st.set_page_config(page_title="Drought Prediction System", layout="wide") # Use wide layout for more space
+ 
 
-
-with streamlit.sidebar:
-    streamlit.title("Drought Prediction")
-    streamlit.header("Project Overview")
-    streamlit.write("""
+with st.sidebar:
+    st.title("🌱 Drought Prediction System")
+    st.header("Project Overview")
+    st.write("""
     Early warning drought prediction system using
     climate features such as temperature, humidity,
     precipitation, solar radiation and SPEI index.
 
     
-    """)
-    streamlit.subheader("🎯 Goal : Support farmers & decision makers in planning")
-    streamlit.divider()
-    streamlit.write("📌 *Enter input values and  click Show result.*")
+     """)
+    st.subheader("🎯 Goal : Support farmers & decision makers in planning")
+    st.divider()
+    st.write("📌 *Enter input values and  click Show result.*")
 
 
-streamlit.title("Drought Prediction System")
+st.title("Drought Prediction System")
+st.markdown("Enter the climate features below")
 
 
 data=pd.read_csv("C:/Users/Sreeraj/OneDrive/Desktop/ML_project/Drought_Prediction/stage_4_drought_dataset.csv")
@@ -47,53 +48,49 @@ print(accuracy_score(y_test,y_pred))
 
 
 
-
-
 def input_features():
-    RH2M= streamlit.number_input("Relative humidity at 2 meters(RH2M) (%)")
+    RH2M= st.number_input("Relative humidity at 2 meters(RH2M) (%)")
                             
                                                    
-    T2M_MAX= streamlit.number_input("Maximum temperature at 2 meters(T2M_MAX) (°C)")
+    T2M_MAX= st.number_input("Maximum temperature at 2 meters(T2M_MAX) (°C)")
                                       
                            
-    T2M_MIN= streamlit.number_input("Minimum temperature at 2 meters(T2M_MIN) (°C)")
+    T2M_MIN= st.number_input("Minimum temperature at 2 meters(T2M_MIN) (°C)")
                                 
                        
-    WS2M = streamlit.number_input("Wind speed at 2 meters(WS2M) (m/s)")
+    WS2M = st.number_input("Wind speed at 2 meters(WS2M) (m/s)")
                                   
                      
-    T2M= streamlit.number_input("Mean temperature at 2 meters above surface(T2M) (°C)")
+    T2M= st.number_input("Mean temperature at 2 meters above surface(T2M) (°C)")
                                      
                        
-    ALLSKY_SFC_SW_DWN=  streamlit.number_input("All Sky Surface Shortwave Downward Irradiance(ALLSKY_SFC_SW_DWN) (W/m² or kW-hr/m²/day)")
+    ALLSKY_SFC_SW_DWN=  st.number_input("All Sky Surface Shortwave Downward Irradiance(ALLSKY_SFC_SW_DWN) (W/m² or kW-hr/m²/day)")
                                                  
                           
-    PRECTOTCORR= streamlit.number_input("Corrected Precipitation(PRECTOTCORR) (mm or mm/day)")
+    PRECTOTCORR= st.number_input("Corrected Precipitation(PRECTOTCORR) (mm or mm/day)")
                                         
              
-    spei=streamlit.number_input("Standardized Precipitation Evapotranspiration Index(SPEI)")
+    spei=st.number_input("Standardized Precipitation Evapotranspiration Index(SPEI)")
                                 
     
-    lat_sin=streamlit.number_input("Latitude Sin(lat_sin)")
+    lat_sin=st.number_input("Latitude Sin(lat_sin)")
     
 
-    lat_cos=streamlit.number_input("Latitude cos(lat_cos)")
+    lat_cos=st.number_input("Latitude cos(lat_cos)")
                                    
 
-    lon_sin=streamlit.number_input("Longitude sin(lon_sin)")
+    lon_sin=st.number_input("Longitude sin(lon_sin)")
                                    
                         
 
-    lon_cos=streamlit.number_input("Longitude cos(lon_cos)")
+    lon_cos=st.number_input("Longitude cos(lon_cos)")
                                    
 
-    month_sin=streamlit.number_input("Month sin(month_sin)")
+    month_sin=st.number_input("Month sin(month_sin)")
                                      
     
-    month_cos=streamlit.number_input("Month cos(month_cos)")
+    month_cos=st.number_input("Month cos(month_cos)")
                                     
-    
-        
 
 
     data={
@@ -112,7 +109,7 @@ def input_features():
         
         "month_sin":month_sin,
         "month_cos":month_cos,
-    }
+     }
 
     features=pd.DataFrame(data,index=[0])
 
@@ -124,7 +121,14 @@ input_scaled=scaler.transform(input_df)
 
 result=model.predict(input_scaled)
 
-if streamlit.button("Show result"):
+
+
+
+
+
+if st.button("Show result"):
     final_result="  NO Drought" if result[0]==0 else " Drought"    
 
-    streamlit.success(final_result)
+    st.success(final_result)
+
+
